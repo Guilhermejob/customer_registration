a0e7b4e0bd996e3a78754534876e30573edb6bda
Guilherme Job <guilhermejob047@gmail.com>
2022-02-18 14:23:58 -0300

    changed the welcome message and route

--- app/routes/principal_blueprint.py
+++ app/routes/principal_blueprint.py
@@ -3,6 +3,6 @@ from flask import Blueprint
 
 from app.controllers.welcome import welcome
 
-bp_principal = Blueprint('bp_clients', __name__, url_prefix='')
+bp_principal = Blueprint('bp_clients', __name__, url_prefix='/')
 
 bp_principal.get('')(welcome)
